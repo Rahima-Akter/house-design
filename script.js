2e0312c6729c3d5ce2b491ee51c513dbae44ed5a
Rahima-Akter <rahimaakter1527@gmail.com>
2024-08-29 05:11:14 -0700

    added work process section

---
+++ script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'banner-bg': "url('./images/banner.png')",
+            },
+            colors: {
+                'primary-btn': '#ABEF5F',
+                'primary-btn2': '#cff2a9',
+                'dark3': '#F3F3F3',
+            },
+        }
+    }
+}
